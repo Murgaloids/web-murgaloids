ff663d35decf2ede759fc8799f012b1827a7c4ad
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-02-20 14:37:40 -0800

    Refactored profile with different item-card component style.

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WindowService } from '../shared/services/window.service';
+
+import { MOBILE_WIDTH } from '../shared/global';
 
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss']
 })
-export class ItemCardComponent {
-
+export class ItemCardComponent implements OnInit {
   @Input() itemName: string;
   @Input() itemAbout: string;
   @Input() itemRating: number;
   @Input() itemPrice: number;
   @Input() itemImageSrc: string;
 
+  private mobileWidth: number;
+
+  constructor(private windowService: WindowService) {}
+
+  ngOnInit() {
+    this.mobileWidth = MOBILE_WIDTH;
+  }
 }
