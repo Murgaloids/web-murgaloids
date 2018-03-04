c3583327b688cb4d7b4ec6bde8887cc3884b9acd
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-03-04 10:50:32 -0800

    Started on footer.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../shared/services/data.service';
-import { Item } from '../shared/models/item.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
-  recentItemsForSale: Item[];
-
-  constructor(private dataService: DataService) { }
-
-  ngOnInit() {
-    this.recentItemsForSale = this.dataService.getRecentItemsForSale();
-  }
-}
+export class HomepageComponent {}
