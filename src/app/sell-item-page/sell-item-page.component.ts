fd100b7b4b29ec8cadfe5584894a3fbb1b9f2d0b
Michael Black <michaelblxck@gmail.com>
2018-03-19 09:00:24 -0700

    Enhance sell item page web murgaloids (#45)
    * added dropdowns and enums
    
    * added a success page

--- src/app/sell-item-page/sell-item-page.component.ts
+++ src/app/sell-item-page/sell-item-page.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataService } from '../shared/services/data.service';
 import { Item } from '../shared/models/item.model';
+import { ItemCondition } from '../shared/global';
+import { ItemCategory } from '../shared/global';
 
 @Component({
   selector: 'app-sell-item-page',
   templateUrl: './sell-item-page.component.html',
-  styleUrls: ['./sell-item-page.component.css']
+  styleUrls: ['./sell-item-page.component.scss']
 })
 export class SellItemPageComponent {
 
   item: Item = new Item();
+  itemCondition: string;
+  itemCategory: string;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
 
   submit() {
+    this.item.sellerId = this.dataService.getUserId();
     this.item.itemSold = false;
+    this.item.rating = 0;
+    this.item.conditionTypeId = ItemCondition[this.itemCondition];
+    this.item.categoryTypeId = ItemCategory[this.itemCategory];
     this.dataService.addNewItem(this.item).subscribe(id => {
+      //the content of this method will change when the
+      //api is updated to return a json object
       localStorage.setItem('item' + id, this.item.imageSrc);
+      this.router.navigate(['/success']);
     });
   }
 
