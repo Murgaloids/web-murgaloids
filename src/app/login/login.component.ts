ac155e5cc12b3ab32fedc815bf6b518531ab76b5
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-04-13 10:42:12 -0700

    changed styling in login and register component. Added load spinner on processing data.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,10 +4,12 @@ import { AuthenticationService } from '../shared/services/authentication.service
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+  ) {}
 
   ngOnInit() {
     this.authenticationService.clearError();
