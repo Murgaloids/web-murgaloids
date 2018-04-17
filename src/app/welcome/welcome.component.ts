b044ac0e793c1bf721ec58b7ef787d0243a8405f
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-04-17 10:41:47 -0700

    Rebase to latest

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-<<<<<<< HEAD
-  styleUrls: ['./welcome.component.css']
-=======
   styleUrls: ['./welcome.component.scss']
->>>>>>> fa932b668c2ccae2f966bf45e26a39e8a8f8ba9f
 })
 export class WelcomeComponent implements OnInit {
 
