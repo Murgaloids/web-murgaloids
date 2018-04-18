9f981f188007c007d507b711a7166f4a3dbcb88f
Michael <michaelblxck@gmail.com>
2018-04-17 23:54:14 -0700

    init

--- src/app/shared/item-card/item-card.component.ts
+++ src/app/shared/item-card/item-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MOBILE_WIDTH } from '../global';
 import { WindowService } from '../services/window.service';
+import { StudentsService } from '../services/students.service';
 import { Item } from '../models/item.model';
 
 @Component({
@@ -12,10 +13,24 @@ export class ItemCardComponent implements OnInit {
   @Input() item: Item;
 
   private mobileWidth: number;
+  private sellerName: string;
+  private ready: boolean;
 
-  constructor(private windowService: WindowService) {}
+  constructor(
+    private windowService: WindowService,
+    private studentsService: StudentsService
+  ) {}
 
   ngOnInit() {
     this.mobileWidth = MOBILE_WIDTH;
+    this.getSellerName(this.item.sellerId);
   }
+
+  getSellerName(id: number):string {
+    this.studentsService.getStudentObservable(id).subscribe(student => {
+        let student = this.studentsService.buildStudent(student, null);
+        console.log(student.name);
+        this.sellerName = student.name;
+        this.ready = true;
+    });
 }
