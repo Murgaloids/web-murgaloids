1204ef9d82607efbf26ed46bb139dc00bd8be841
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-04-19 17:17:22 -0700

    My changes (#59)
    * comitting changes.
    
    * Rebase to latest
    
    * rebase to latest
    
    * Enhanced css for item-card component, Also implemented edit profile page.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../shared/services/authentication.service';
 import { StudentsService } from '../shared/services/students.service';
 import { ItemsService } from '../shared/services/items.service';
 import { Student } from '../shared/models/student.model';
@@ -8,24 +9,25 @@ import { Item } from '../shared/models/item.model';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
-  providers: [ StudentsService ]
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
   id: number;
   student: Student;
   ready: boolean = false;
-
+  
   constructor(
     private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
     private studentsService: StudentsService,
     private itemsService: ItemsService
   ) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.studentsService.getStudentObservable(+params['id']).subscribe(student => {
-        this.itemsService.getUserItemsObservable(+params['id']).subscribe(item => {
+      this.id = +params['id'];
+      this.studentsService.getStudentObservable(this.id).subscribe(student => {
+        this.itemsService.getUserItemsObservable(this.id).subscribe(item => {
           let itemsForSale: Item[] = this.itemsService.buildItemsForSale(item.data);
           this.student = this.studentsService.buildStudent(student, itemsForSale);
           this.ready = true;
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../shared/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    if (this.authenticationService.token)
+      this.router.navigate(['/home']);
   }
-
 }
