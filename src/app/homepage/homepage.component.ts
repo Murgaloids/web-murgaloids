37c5d0ab17b3da1ad8f9ddfb726f8f450a23e0b0
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-04-24 14:45:09 -0700

    Implemented displayMoreItem for homepage component.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,7 +12,8 @@ const NUM_OF_ITEMS_TO_FETCH = 10;
 })
 
 export class HomepageComponent implements OnInit {
-  recentItemsForSale: Item[];
+  private recentItemsForSale: Item[];
+  private itemCount = 8;
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -21,6 +22,11 @@ export class HomepageComponent implements OnInit {
 
   ngOnInit() {
     this.itemsService.setRecentItems(NUM_OF_ITEMS_TO_FETCH)
-      .then(() => this.recentItemsForSale = this.itemsService.recentItems.slice(0, 8));
+      .then(() => this.recentItemsForSale = this.itemsService.recentItems.slice(0, this.itemCount));
+  }
+
+  displayMoreItems() {
+    this.itemCount += 8;
+    this.recentItemsForSale = this.itemsService.recentItems.slice(0, this.itemCount);
   }
 }
