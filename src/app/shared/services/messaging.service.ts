909f90c931813fc8d664b4c289cc3e6ffe78d9aa
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-04-24 17:17:56 -0700

    Initialized post request for messages and conversations.

--- src/app/shared/services/messaging.service.ts
+++ src/app/shared/services/messaging.service.ts
@@ -24,4 +24,44 @@ export class MessagingService {
       }
     });
   }
+
+  addConversationWithInitialMessage(studentId: number, message: string) {
+    return new Promise((resolve, reject) => {
+      if (studentId >= 0 && message) {
+        const bodyObject: object = {
+          id: [this.authenticationService.userId, studentId].sort().join(':'),
+          student1Id: this.authenticationService.userId,
+          student2Id: studentId
+        };
+
+        const headers = {'Authorization': this.authenticationService.token};
+        this.http.post(`${SERVER_URL}/conversations/add`, bodyObject, {headers, observe: 'response'})
+          .subscribe(
+            this.addMessage.bind(this, message, resolve, reject),
+            err => reject(err)
+          );
+      }
+    });
+  }
+
+  addMessage(message, resolve, reject, res: any) {
+    if (res && res.body) {
+      const bodyObject: object = {
+        conversationId: res.body.data.id,
+        senderId: this.authenticationService.userId,
+        message: message
+      };
+
+      const headers = {'Authorization': this.authenticationService.token};
+      this.http.post(`${SERVER_URL}/messages/add`, bodyObject, {headers, observe: 'response'})
+        .subscribe(
+          (res: any) => {
+            if (res && res.body) {
+              resolve(res.body.data);
+            } else resolve(null);
+          },
+          err => reject(err)
+        );
+    }
+  }
 }
