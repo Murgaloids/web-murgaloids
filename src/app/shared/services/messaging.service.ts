25aa8d1ae24d2a1e5f498704805bdce8dd6d3884
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-05-01 14:06:00 -0700

    Rebase to latest

--- src/app/shared/services/messaging.service.ts
+++ src/app/shared/services/messaging.service.ts
@@ -99,7 +99,6 @@ export class MessagingService {
                 },
                 messages: [messageRes.body.data]
               };
-
               resolve(messageRes.body.data);
           } else reject(null);
         },
