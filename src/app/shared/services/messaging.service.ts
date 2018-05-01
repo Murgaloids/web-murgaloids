3b7ae835af67a9ff5dea517e8ed3fc6b3bdc3b81
Derrick Nguyen <derrickhnguyen@outlook.com>
2018-05-01 14:05:16 -0700

    Rebase to latest

--- src/app/shared/services/messaging.service.ts
+++ src/app/shared/services/messaging.service.ts
@@ -86,7 +86,6 @@ export class MessagingService {
       };
 
       const headers = {'Authorization': this.authenticationService.token};
-
       this.http.post(`${SERVER_URL}/messages/add`, bodyObject, {headers, observe: 'response'})
         .subscribe(
           (messageRes: any) => {
@@ -101,7 +100,6 @@ export class MessagingService {
 
   public getConversations(): void {
     const headers = {'Authorization': this.authenticationService.token};
-
     this.http.get(`${SERVER_URL}/conversations/get-conversations?id=${this.authenticationService.userId}`, {headers})
       .subscribe((res: any) => {
         if (res && res.data) this.mConversations = res.data;
